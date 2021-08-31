4bd092e6f0f0668505bfda3967de8a753fcc8431
ilyaYarosh88 <iyarosh88@mail.ru>
2021-08-31 20:31:29 +0300

    Сделали помощник сессии

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -14,14 +14,14 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="Test_name", header="Test_header", footer="Test_footer"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
 
 
