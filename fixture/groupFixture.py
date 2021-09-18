a4b76410045f5608086e6311a96c83a991921780
ilyaYarosh88 <iyarosh88@mail.ru>
2021-09-18 21:49:59 +0300

    делаем гет груп лист

--- fixture/groupFixture.py
+++ fixture/groupFixture.py
@@ -68,3 +68,10 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.get_text()
+            id = element.find_element_by_name("selected[]").get_attribute("value")
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,7 +2,10 @@
 from model.group import Group
 
 def test_add_group(app):
+    old_groups = app.groupFixture.get_group_list()
     app.groupFixture.create(Group(name="Test_name", header="Test_header", footer="Test_footer"))
+    new_groups = app.groupFixture.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
