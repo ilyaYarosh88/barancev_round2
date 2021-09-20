2ab1791c302530aba5a22bec2c923c48a6b1407a
ilyaYarosh88 <iyarosh88@mail.ru>
2021-09-20 22:38:48 +0300

    Сортировка списков перед сравнением

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -9,19 +9,16 @@ def test_add_group(app):
     new_groups = app.groupFixture.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
-    def id_or_max(gr):
-        if gr.id:
-            return int(gr.id)
-        else:
-            return maxsize
-    assert sorted(old_groups, key=id_or_max) == sorted(new_groups, key=id_or_max)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_add_empty_group(app):
     old_groups = app.groupFixture.get_group_list()
-    app.groupFixture.create(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    app.groupFixture.create(group)
     new_groups = app.groupFixture.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
-
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
