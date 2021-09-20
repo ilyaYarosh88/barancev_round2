9c3bbaa2e92139966d6606d564aa07b46bf523f5
ilyaYarosh88 <iyarosh88@mail.ru>
2021-09-20 22:50:51 +0300

    Проверка списков в редактировании групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -6,9 +6,15 @@ def test_modify_group_name(app):
     if app.groupFixture.group_count() == 0:
         app.groupFixture.create(Group(name="Test_name", header="Test_header", footer="Test_footer"))
     old_groups = app.groupFixture.get_group_list()
-    app.groupFixture.edit_first_group(Group(name="EditedName"))
+    group = Group(name="EditedGroup", header="EditedHeader", footer="EditedFooter")
+    group.id = old_groups[0].id
+    app.groupFixture.edit_first_group(group)
     new_groups = app.groupFixture.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+
 
 
 
